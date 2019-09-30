444be0850efd4ef52530617aa81274ae6f74444d
lgarridz <lgarridz@corp.vodafone.es>
2019-09-30 17:38:43 +0200

    inital project done with Spring and Angular

---
+++ src/main/java/com/webApi/BackAtareado/BackAtareadoApplication.java
@@ -0,0 +1,13 @@
+package com.webApi.BackAtareado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackAtareadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackAtareadoApplication.class, args);
+	}
+
+}
